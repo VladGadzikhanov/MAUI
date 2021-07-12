42dd18dfc43687f868328af9f8d90b9d974d2ac7
Владислав Гаджиханов <vladislavgadzihanov@MacBook-Pro-Vladislav.local>
2021-07-12 22:13:51 +0300

    Insertion ef

---
+++ HelloMaui/Authorization.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloMaui
+{
+    public class Authorization : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public Authorization()
+          : base()
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().Property(x => x.UserId).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.FirstName).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.LastName).IsRequired();
+            modelBuilder.Entity<User>().Property(x => x.Password).IsRequired();
+        }
+    }
+}
--- HelloMaui/Startup.cs
+++ HelloMaui/Startup.cs
@@ -2,11 +2,61 @@ using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.LifecycleEvents;
+using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace HelloMaui
 {
 	public class Startup : IStartup
 	{
+        public void Execute()
+        {
+            string connectionString = "server=localhost;port=3306;database=HelloMaui;uid=root";
+
+            using MySqlConnection connection = new MySqlConnection(connectionString);
+            // Create database if not exists
+            using (Authorization contextDB = new())
+            {
+                contextDB.Database.RollbackTransactionAsync();
+            }
+
+            connection.Open();
+            MySqlTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                // DbConnection that is already opened
+                using (Authorization context = new Authorization())
+                {
+
+                    // Interception/SQL logging
+
+                    // Passing an existing transaction to the context
+                    context.Database.UseTransaction(transaction);
+
+                    // DbSet.AddRange
+                    List<User> users = new();
+
+                    users.Add(new User { FirstName = "Vlad", LastName = "Gadzikhanov", Password = "0807" });
+                    users.Add(new User { FirstName = "Anya", LastName = "Khromova", Password = "0000" });
+                    users.Add(new User { FirstName = "Ihor", LastName = "Bailov", Password = "1111" });
+                    users.Add(new User { FirstName = "Ivan", LastName = "Ivanov", Password = "2222" });
+
+                    context.Users.AddRange(users);
+
+                    context.SaveChanges();
+                }
+
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
 		public void Configure(IAppHostBuilder appBuilder)
 		{
 			appBuilder
@@ -23,6 +73,7 @@ namespace HelloMaui
 					});
 					#endif
 				});
+            Execute();
 		}
 	}
 }
